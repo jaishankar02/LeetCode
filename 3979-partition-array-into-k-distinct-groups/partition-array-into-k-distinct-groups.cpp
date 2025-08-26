f39e62c6e2f6eb754526f0b92abac33cd4f6a988
jaishankar02 <92317436+jaishankar02@users.noreply.github.com>
2025-08-26 12:10:35 +0530

    Time: 223 ms (11.24%) | Memory: 336.2 MB (5.53%) - LeetSync

--- 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
@@ -5,22 +5,15 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        long long sum=0, maxele=0;
+        int maxele=0;
         for(auto it:mp){
             if(it.second>maxele){
-                sum+=maxele;
                 maxele=it.second;
-                
             }
-            else sum+=it.second;
-
-            // cout<<sum<<endl;
         }
-        cout<<sum<<endl;
-        long long s=sum-(maxele*(k-1));
-        cout<<s<<endl;
-        if(s<0) return false;
-        if(s%k==0) return true;
-        else return false;
+        if(nums.size()%k!=0) return false;
+        int holes=nums.size()/k;
+        if(maxele>holes) return false;
+        return true;
     }
 };
